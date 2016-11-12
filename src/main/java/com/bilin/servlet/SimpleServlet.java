2ac08e4263a6ae437bed8550f1675759c6d3942d
Bilin BS <Bilin BS>
2016-11-12 23:37:56 +0530

    Added login functionality

---
+++ src/main/java/com/bilin/servlet/SimpleServlet.java
@@ -0,0 +1,58 @@
+package com.bilin.servlet;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public SimpleServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().setAttribute("userName", null);
+		response.getWriter().append("You are now logged out. <br/>")
+		.append("<hr/")
+		.append("<a href=\"/webapp\">Home</a>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String userName =  request.getParameter("txtname");
+		String password =  request.getParameter("txtpass");
+		if(authenticate(userName, password)){
+		    session.setAttribute("userName", userName);
+		    request.getRequestDispatcher("index.jsp").forward(request, response);
+		} else {
+		    request.setAttribute("errMsg", "Invalid Credentials!");
+		    request.getRequestDispatcher("start.jsp").forward(request, response);
+		}
+		
+	}
+
+    private boolean authenticate(String userName, String password) {
+        return !userName.isEmpty() && new StringBuffer(userName.toLowerCase()).reverse().toString().equals(password);
+    }
+
+}
---
+++ target/webapp/start.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Hudson Demo</title>
+</head>
+<body>
+<a href="/webapp/index.jsp">Hello World</a>
+<hr/>
+<form action="/webapp/greeting" method="post">
+		<table cellpadding="10">
+			<% String errMsg = (String)request.getAttribute("errMsg");
+			if(errMsg != null){
+			    out.println(errMsg);
+			}
+			%>
+			<tr>
+				<td>Username</td>
+				<td><input type="text" name="txtname"/></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="password" name="txtpass"/></td>
+			</tr>
+			<tr>
+				<td><input type="submit" value="Login"/></td>
+				<td></td>
+			</tr>
+		</table>
+	</form>
+	<hr/>
+</body>
+</html>
